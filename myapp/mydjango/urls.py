c327a57ac07f837583311cc0f543a47b526b9fb9
astechutube <astech.utube@gmail.com>
2022-02-12 21:58:26 +0530

    django fresh app

--- myapp/mydjango/urls.py
+++ myapp/mydjango/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from . import views
+#from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index, name='index'),
-    path('about', views.about, name='about'),
+    #path('', views.index, name='index'),
+    #path('about', views.about, name='about'),
 ]
+
+
