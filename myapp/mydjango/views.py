1db02956f701f30bac6e3900d04bd007b23f51c9
astechutube <astech.utube@gmail.com>
2022-02-12 22:03:17 +0530

    django fresh app

--- myapp/mydjango/views.py
+++
@@ -1,7 +0,0 @@
-from django.http import HttpResponse
-
-def index(request):
-    return HttpResponse("<h1>This is index function.</h1>")
-
-def about(request):
-    return HttpResponse("This is about function.")    
